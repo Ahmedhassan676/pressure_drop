f45443072bd001f728598d0827f25f4e49e79971
Ahmedhassan676 <81989422+Ahmedhassan676@users.noreply.github.com>
2023-03-20 17:23:25 +0200

    Update line.py

--- line.py
+++ line.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 import neqsim
 from neqsim.thermo.thermoTools import *
 from neqsim.process.processTools import *
